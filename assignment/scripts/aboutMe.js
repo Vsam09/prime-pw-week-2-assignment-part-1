651fa9b76b83d9b008474c8703cc148771dc89e0
samson vang <vangsam2@gmail.com>
2021-06-17 20:33:58 -0500

    commit#4

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -23,14 +23,15 @@ console.log('My favorite food is:', food, foodVar );
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
 let pets = 0;
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-
+let friendsPets = 1;
 // 11 - Add two pets to your `pets` variable
-
+pets = 2;
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-
+const allowedPets = 0;
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!",
 // if it's not true, console log "How about we stay home?"
-
+adventurous = true;
+console.log('Adventures are great!')
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
